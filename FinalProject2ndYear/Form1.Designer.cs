b5981334695e6fe156765406ba2d6117932e70c8
Patryk John Guantero <guantero.290593@balagtas.sti.edu.ph>
2026-04-15 14:43:00 +0800

    Add project files.

---
+++ FinalProject2ndYear/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace FinalProject2ndYear
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.LoginPanel = new System.Windows.Forms.Panel();
+            this.LoginButton = new System.Windows.Forms.Button();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.UsernameLbl = new System.Windows.Forms.Label();
+            this.PasswordLbl = new System.Windows.Forms.Label();
+            this.UsernameTextBox = new System.Windows.Forms.TextBox();
+            this.LoginLabel = new System.Windows.Forms.Label();
+            this.LoginPanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LoginPanel
+            // 
+            this.LoginPanel.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.LoginPanel.Controls.Add(this.LoginLabel);
+            this.LoginPanel.Controls.Add(this.LoginButton);
+            this.LoginPanel.Controls.Add(this.PasswordTextBox);
+            this.LoginPanel.Controls.Add(this.UsernameLbl);
+            this.LoginPanel.Controls.Add(this.PasswordLbl);
+            this.LoginPanel.Controls.Add(this.UsernameTextBox);
+            this.LoginPanel.Location = new System.Drawing.Point(280, 95);
+            this.LoginPanel.Name = "LoginPanel";
+            this.LoginPanel.Size = new System.Drawing.Size(249, 255);
+            this.LoginPanel.TabIndex = 0;
+            // 
+            // LoginButton
+            // 
+            this.LoginButton.Location = new System.Drawing.Point(85, 192);
+            this.LoginButton.Name = "LoginButton";
+            this.LoginButton.Size = new System.Drawing.Size(75, 23);
+            this.LoginButton.TabIndex = 5;
+            this.LoginButton.Text = "LOGIN";
+            this.LoginButton.UseVisualStyleBackColor = true;
+            this.LoginButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Location = new System.Drawing.Point(74, 140);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordTextBox.TabIndex = 3;
+            // 
+            // UsernameLbl
+            // 
+            this.UsernameLbl.AutoSize = true;
+            this.UsernameLbl.Location = new System.Drawing.Point(71, 85);
+            this.UsernameLbl.Name = "UsernameLbl";
+            this.UsernameLbl.Size = new System.Drawing.Size(58, 13);
+            this.UsernameLbl.TabIndex = 0;
+            this.UsernameLbl.Text = "Username:";
+            // 
+            // PasswordLbl
+            // 
+            this.PasswordLbl.AutoSize = true;
+            this.PasswordLbl.Location = new System.Drawing.Point(71, 124);
+            this.PasswordLbl.Name = "PasswordLbl";
+            this.PasswordLbl.Size = new System.Drawing.Size(56, 13);
+            this.PasswordLbl.TabIndex = 1;
+            this.PasswordLbl.Text = "Password:";
+            // 
+            // UsernameTextBox
+            // 
+            this.UsernameTextBox.Location = new System.Drawing.Point(74, 101);
+            this.UsernameTextBox.Name = "UsernameTextBox";
+            this.UsernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.UsernameTextBox.TabIndex = 2;
+            // 
+            // LoginLabel
+            // 
+            this.LoginLabel.AutoSize = true;
+            this.LoginLabel.Location = new System.Drawing.Point(71, 46);
+            this.LoginLabel.Name = "LoginLabel";
+            this.LoginLabel.Size = new System.Drawing.Size(108, 13);
+            this.LoginLabel.TabIndex = 6;
+            this.LoginLabel.Text = "Log-In to StockTrack";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LoginPanel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Vitarich StockTrack";
+            this.LoginPanel.ResumeLayout(false);
+            this.LoginPanel.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel LoginPanel;
+        private System.Windows.Forms.Button LoginButton;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label UsernameLbl;
+        private System.Windows.Forms.Label PasswordLbl;
+        private System.Windows.Forms.TextBox UsernameTextBox;
+        private System.Windows.Forms.Label LoginLabel;
+    }
+}
+
---
+++ FinalProject2ndYear/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalProject2ndYear
+{
+    public partial class Form1 : Form
+    {
+       
+    public Form1()
+        {
+          
+            InitializeComponent();
+        }
+
+    private void UserLogin()
+        {
+            string connStr = ConfigurationManager.ConnectionStrings["DBConn"].ConnectionString;
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                String queryLogin = "SELECT COUNT(*) FROM Admin_Accounts WHERE username = @username AND password = @password";
+                SqlCommand cmd = new SqlCommand(queryLogin, conn);
+
+                cmd.Parameters.AddWithValue("@username", UsernameTextBox.Text);
+                cmd.Parameters.AddWithValue("@password", PasswordTextBox.Text);
+
+                int result = (int)cmd.ExecuteScalar();
+
+                if (result == 0)
+                {
+                    MessageBox.Show("Incorrect Login");
+
+                }
+                else if (result >= 1)
+                {
+                    MessageBox.Show("Login Successful!");
+                    LoginPanel.Visible = false;
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+         UserLogin();
+
+        }
+    }
+}
